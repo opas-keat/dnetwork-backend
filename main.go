4f84d2d9272e68cb5cd41d64a1cd10cfff5091ca
opas.keat <opas.keat@gmail.com>
2022-09-09 17:44:17 +0700

    add testify

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 var version = "dev"
 
-func main() {
-
-	fmt.Printf("Version: %s\n", version)
-
-	fmt.Println(hello())
+func Greeting() string {
+	return "Hello"
 }
 
-func hello() string {
-	return "Hello Golang"
+func GoodBye(name string) string {
+	text := "Goodbye, " + name + "..."
+	return text
+}
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	http.HandleFunc("/", GreetingAPI)
+	http.ListenAndServe(":3000", nil)
+}
+
+func GreetingAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Gopher")
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,99 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestHello(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
-	want := "Hello Golang"
+func TestGreeting(t *testing.T) {
+	expectedResult := "Hello"
+	receivedResult := Greeting()
+	assert.Equal(t, expectedResult, receivedResult)
+}
 
-	got := hello()
+func TestGoodBye(t *testing.T) {
+	expectedResult := "Goodbye, John..."
+	receivedResult := GoodBye("John")
+	assert.Equal(t, expectedResult, receivedResult)
+}
 
-	if want != got {
-		t.Fatalf("want %s, got %s\n", want, got)
+func TestAdd(t *testing.T) {
+	type testCase struct {
+		a        int
+		b        int
+		expected int
+	}
+
+	testCases := []testCase{
+		{a: 2, b: 2, expected: 4},
+		{a: 1, b: 4, expected: 5},
+	}
+
+	for _, test := range testCases {
+		expectedResult := test.expected
+		receivedResult := Add(test.a, test.b)
+		assert.Equal(t, expectedResult, receivedResult)
+
+		fmt.Printf("case : a = %d, b = %d while expected result equals to %d, "+
+			"received result equals to %d\n", test.a, test.b, test.expected, receivedResult)
+	}
+}
+
+func TestAddWithTableDrivenTesting(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+
+	type testCase struct {
+		name string
+		args args
+		want int
+	}
+
+	tests := []testCase{
+		{
+			name: "should adding correctly a and b when they are equal",
+			args: args{a: 2, b: 2},
+			want: 4,
+		},
+		{
+			name: "should adding correctly a and b when a is less than b",
+			args: args{a: 1, b: 3},
+			want: 4,
+		},
+		{
+			name: "should adding correctly a and b when a is bigger than b",
+			args: args{a: 5, b: 3},
+			want: 8,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equalf(t, test.want, Add(test.args.a, test.args.b), "Add(%v, %v)", test.args.a, test.args.b)
+		})
+	}
+}
+
+func TestGreetingAPI(t *testing.T) {
+	// Initialize a new dummy http.Request.
+	req, _ := http.NewRequest(http.MethodGet, "/", http.NoBody)
+	rec := httptest.NewRecorder()
+
+	// Call the GreetingAPI handler function, passing in the
+	// httptest.ResponseRecorder and http.Request.
+	GreetingAPI(rec, req)
+
+	expected := "Hello Gopher"
+	received := rec.Body.String()
+
+	// We can also use `assert.Equal(t, expected, actual)`
+	if expected != received {
+		t.Errorf("expected %s must equal to received %s", expected, received)
 	}
 }
